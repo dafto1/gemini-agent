38eaec3741b636f635915f8a78ec8f43f2fcb150
dafto1 <shreyashnikam1005@gmail.com>
2026-04-30 14:58:04 +0530

    feat : added run python files tool

--- tests.py
+++ tests.py
@@ -1,10 +1,9 @@
 from functions.get_files_info import get_files_info 
 from functions.get_file_content import get_file_content 
 from functions.write_file import write_file
+from functions.run_python import run_python_file 
 def main() : 
     working_dir = "calculator" 
    
-    write_file("calculator", "lorem.txt", "wait, this is not lorem ipsum")  
-    print(get_file_content(working_dir ,"lorem.txt")) 
-    write_file("calculator", "pkg/tmp/new.txt", "wait this is a new lorem ipsum") 
+    print(run_python_file(working_dir, "main.py" , ["3 + 5"]))
 main()
