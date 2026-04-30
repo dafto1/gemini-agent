fc13758c7347ab89cf8ef6b5907feb2d8615d2e6
dafto1 <shreyashnikam1005@gmail.com>
2026-04-30 14:36:08 +0530

    feat : write files tool

--- tests.py
+++ tests.py
@@ -1,7 +1,10 @@
 from functions.get_files_info import get_files_info 
 from functions.get_file_content import get_file_content 
+from functions.write_file import write_file
 def main() : 
     working_dir = "calculator" 
+   
+    write_file("calculator", "lorem.txt", "wait, this is not lorem ipsum")  
     print(get_file_content(working_dir ,"lorem.txt")) 
-
+    write_file("calculator", "pkg/tmp/new.txt", "wait this is a new lorem ipsum") 
 main()
