a2fd47b1457e5bc03edc9f7edc64eeddf3b5151b
dafto1 <shreyashnikam1005@gmail.com>
2026-04-30 12:35:13 +0530

    feat : added get files info tool

--- main.py
+++ main.py
@@ -3,26 +3,42 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
+from functions.get_files_info import get_files_info 
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    
     if len(sys.argv) < 2:
         print("I need a prompt")
         sys.exit(1)
     prompt = sys.argv[1]
+    verbose_flag = False 
 
-    response = client.models.generate_content(model="gemma-4-31b-it", contents=prompt)
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose" : 
+        verbose_flag = True  
+        
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+
+    response = client.models.generate_content(
+        model="gemma-4-31b-it",
+        contents=messages
+     )
 
     print(response.text)
-    if response.usage_metadata:
-        print(f"Prompt tokens : {response.usage_metadata.prompt_token_count}")
+    if response is None or response.usage_metadata is None : 
+        print("Something is wrong with the response !") ;  
+        return  
+    if verbose_flag : 
+        print(f"User prompt : {prompt}" )
+        print(f"Prompt tokens :{response.usage_metadata.prompt_token_count}") 
         print(f"Response tokens : {response.usage_metadata.candidates_token_count}")
-    else:
-        print("Usage metadata not available")
 
 
-if __name__ == "__main__":
-    main()
+print(get_files_info("calculator" , "pkg"))
+ 
+# if __name__ == "__main__":
+#     main()
